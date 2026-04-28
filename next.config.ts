f430e8717c9e762b84d0f4fe3ae114b70189d0fb
Davut Kundura Admin <admin@davutkundura.com>
2026-04-28 19:29:49 +0300

    tertemiz

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Vercel build asamasinda ESLint hatalarini gormezden gel.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Vercel build asamasinda TypeScript (Tip) hatalarini gormezden gel.
-    ignoreBuildErrors: true,
-  },
-  images: {
-    // Resimlerin gosteriminde dis baglantilara acik ol (hata vermesin)
-    remotePatterns: [
-      { protocol: "https", hostname: "**" },
-      { protocol: "http", hostname: "**" }
-    ],
-  }
+  /* config options here */
 };
 
 export default nextConfig;
